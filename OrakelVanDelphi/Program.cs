86aa0c6ede0a5bf17c100d83422a29dd80ea2961
igor.smirnov@telenet.be <igor.smirnov@telenet.be>
2022-01-10 11:41:40 +0100

    Added age & sex  params (ex. from part 5 decissions)

--- OrakelVanDelphi/Program.cs
+++ OrakelVanDelphi/Program.cs
@@ -9,12 +9,51 @@ namespace OrakelVanDelphi
 {
     class Program
     {
+        const string inputError = "Invoer bevat de fout. Probeer het opnieuw...";
+        const string inputGeslacht = "Bent u een vrouw(v) of een man(m) ?";
+        const string inputLeeftijdVraag = "Tik uw leeftijd aub ?";
+        const int leeftijdVrouwMax = 150;
+        const int leeftijdManMax = 120;
+        public static void Orakeltje(char geslachtChar, int leeftijdValue)
+        {
+            Random rd_Orakel = new Random();
+            if (geslachtChar == 'v' || geslachtChar == 'V')
+            {
+                Console.WriteLine($"Je zal nog {rd_Orakel.Next(5, leeftijdVrouwMax - leeftijdValue)} jaar leven.");
+            }
+            else if (geslachtChar == 'm' || geslachtChar == 'M')
+            {
+                Console.WriteLine($"Je zal nog {rd_Orakel.Next(5, leeftijdManMax - leeftijdValue)} jaar leven.");
+            }
+            else
+            {
+                Console.WriteLine(inputError);
+            }
+        }
         static void Main(string[] args)
         {
-            /// Cc
-            Random rd_Orakel = new Random();
-            Console.WriteLine ($"Je zal nog {rd_Orakel.Next(5,125)} jaar leven.");
-            Console.WriteLine(" Hello ");
+            ConsoleKeyInfo cki_Key;
+            int inputLeeftijd;
+            bool inputCorrect = false;
+            while(!inputCorrect)
+            try
+            {
+                Console.Write(inputGeslacht);
+                cki_Key = Console.ReadKey();
+                Console.WriteLine();
+                Console.Write(inputLeeftijdVraag);
+                inputLeeftijd = int.Parse(Console.ReadLine());
+                Orakeltje(cki_Key.KeyChar, inputLeeftijd);
+                inputCorrect = true;
+            }
+            catch (Exception e)
+            { 
+                    inputCorrect = false;
+                    Console.WriteLine(inputError);
+
+            }
+            
+
         }
     }
 }
