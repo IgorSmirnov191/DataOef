689b89f70125ff3496df924824b0c520fb27cc85
igor.smirnov@telenet.be <igor.smirnov@telenet.be>
2022-01-10 20:33:13 +0100

    Added exc. Quiz (part 5, deel 14), EnumSeizoenen (part 5, deel 17)

---
+++ EnumSeizoenen/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumSeizoenen
+{
+    internal class Program
+    {
+        enum Seizonsgroepen { koude, warme};
+        enum Seizoenen { winter, lente, zomer, herfst };
+        enum Maanden {januari = 1, februari, maart, april, mei, juni, juli, augustus, september, oktober, november, december};
+        const string inputError = "Invoer bevat de fout. Probeer het opnieuw...";
+        static void Main(string[] args)
+        {
+            string[] seisonsgroenen = { "koude", "varme"};
+            string[] maanden = { "none", "januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"};
+            string[] seizoenen = { "winter"," lente","zomer","herfst"};
+            bool inputExit = false;
+            Console.WriteLine("Tik 0 om te kunnen stoppen");
+            while (!inputExit)
+                try
+                {
+                    Console.Write("Voer in maandnummer aub ?");
+                    int maand = int.Parse(Console.ReadLine());
+                    switch(maand)
+                    {
+
+                        case 0 :
+                        { 
+                                inputExit = true;
+                                Console.WriteLine("Bye");
+                                break;
+                        }
+                        case 12:
+                        case 1 :
+                        case 2:
+                        {
+                                Console.WriteLine($"Maand {maanden[maand]} is een {seisonsgroenen[0]} maand van {seizoenen[0]} seizoen ");
+                                break;
+                        }
+                        case 3:
+                        case 4:
+                        case 5:
+                        {
+                                Console.WriteLine($"Maand {maanden[maand]} is een {seisonsgroenen[1]} maand van {seizoenen[1]} seizoen ");
+                                break;
+                        }
+                        case 6:
+                        case 7:
+                        case 8:
+                        {
+                                Console.WriteLine($"Maand {maanden[maand]} is een {seisonsgroenen[1]} maand van {seizoenen[2]} seizoen ");
+                                break;
+                        }
+                        case 9:
+                        case 10:
+                        case 11:
+                        {
+                                Console.WriteLine($"Maand {maanden[maand]} is een {seisonsgroenen[0]} maand van {seizoenen[3]} seizoen ");
+                                break;
+
+                        }
+                        default:
+                        {
+                                inputExit = false;
+                                Console.WriteLine(inputError);
+                                break;
+
+                        }
+                    }
+            
+                }
+                catch (Exception e)
+                {
+                    inputExit = false;
+                    Console.WriteLine(inputError);
+
+                }
+
+        }
+    }
+}
---
+++ Quiz/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    internal class Program
+    {
+        static int QuizResult(string correctAntwoord)
+        {
+            int result;
+            ConsoleKeyInfo cki_Key = Console.ReadKey();
+
+            switch (cki_Key.Key)
+            {
+                case ConsoleKey.A: //1000
+                    {
+                        result = ("1000" == correctAntwoord) ? 2 : -1;
+                        break;
+                    }
+                case ConsoleKey.B: //0100
+                    {
+                        result = ("0100" == correctAntwoord) ? 2 : -1;
+                        break;
+                    }
+                case ConsoleKey.C://0010
+                    {
+                        result = ("0010" == correctAntwoord) ? 2 : -1;
+                        break;
+                    }
+                case ConsoleKey.D: //0001
+                    {
+                        result = ("0001" == correctAntwoord) ? 2 : -1;
+                        break;
+                    }
+                default:
+                    {
+                        result = 0;
+                        break;
+                    }
+
+            }
+            return result;
+        }
+        
+        static void Main(string[] args)
+        {
+            string[] quizAntwoord = { "0100", "1000", "0001" };
+            int result = 0;
+           
+            for (int i = 0; i < 3; i++)
+            { 
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red + i;
+                Console.WriteLine($"Voer het antwoord op vraag n.{i+1} (a,b,c of d) ? ");
+                result += QuizResult(quizAntwoord[i]);
+            }
+            Console.Clear();
+            Console.ResetColor();
+            Console.WriteLine($"U bent op {result} punten gescoord");
+
+        }
+    }
+}
