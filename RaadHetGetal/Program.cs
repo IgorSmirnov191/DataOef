73e4831dd6166c458a314e25afaa33f22a271266
igor.smirnov@telenet.be <igor.smirnov@telenet.be>
2022-01-12 18:38:06 +0100

    Fixed RaadHetGetal deel 2,3,4 in part 6. Herhalen

--- RaadHetGetal/Program.cs
+++ RaadHetGetal/Program.cs
@@ -8,35 +8,55 @@ namespace RaadHetGetal
 {
     internal class Program
     {
+        const string errorBuitenBereik = "Oops. Het poging is buiten bereik";
         static void Main(string[] args)
         {
-            int getal;
-            int poging = 0;
-            int aantalPogingen = 0;
-            string pogingString;
-            bool gevonden = false;
+            
+            int getal=0, poging = 0, aantalPogingen = 0;
+            int lowrand = 0, highrand = 100, intervaal = highrand;
+            int maxAantalPogingen = 0;
+            while ((intervaal=intervaal/2) != 0)
+            { 
+                maxAantalPogingen++; 
+            } ;
             Random rand = new Random();
-            getal = rand.Next(0, 10);
-
-            while (!gevonden)
+            getal = rand.Next(lowrand, highrand);
+            bool gevonden = false;
+            while ((aantalPogingen < maxAantalPogingen) && !gevonden)
             {
-                aantalPogingen++;
-                Console.WriteLine("Geef een getal tussen 0 en 10");
-                pogingString = Console.ReadLine();
-                poging = int.Parse(pogingString);
-
+                Console.WriteLine($"Geef een getal tussen {lowrand} en {highrand}. U hebt nog {maxAantalPogingen-aantalPogingen} pogingen");
+                poging = int.Parse(Console.ReadLine());
                 if (getal > poging)
                 {
-                    Console.WriteLine("Het gezochte getal is groter, probeer opnieuw.");
+                    if (poging < highrand && poging >= lowrand)
+                    {
+                        lowrand = poging;
+                        Console.WriteLine($"Het gezochte getal is groter dan invoer, het moet groter dan {poging} zijn maar kleiner dan {highrand}, probeer opnieuw.");
+                        aantalPogingen++;
+                    }
+                    else
+                        Console.WriteLine(errorBuitenBereik);
+
                 }
                 else if (getal < poging)
                 {
-                    Console.WriteLine("Het gezochte getal is kleiner, probeer opnieuw.");
+                    if (poging > lowrand && poging <= highrand)
+                    {
+                        highrand = poging;
+                        Console.WriteLine($"Het gezochte getal is kleiner dan invoer, moet kleiner dan {poging} zijn maar groter dan {lowrand}, probeer opnieuw.");
+                        aantalPogingen++;
+                    }
+                    else
+                        Console.WriteLine(errorBuitenBereik);
                 }
                 else
+                {
                     gevonden = true;
+                    aantalPogingen++;
+                }
             }
-            Console.WriteLine($"Gevonden! Het te zoeken getal was inderdaad {getal} je had er {aantalPogingen} pogingen voor nodig.");
+            if (gevonden) Console.WriteLine($"Gevonden! Het te zoeken getal was inderdaad {getal} je had er {aantalPogingen} pogingen voor nodig.");
+            else Console.WriteLine("Oops. U hebt geen pogingen meer");
         }
     }
 }
