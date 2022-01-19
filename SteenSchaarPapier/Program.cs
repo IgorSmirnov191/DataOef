ae2f946c2d27b76fc454e07b4dd62f563471d0a7
igor.smirnov@telenet.be <igor.smirnov@telenet.be>
2022-01-19 12:40:38 +0100

    Fixed oef SteenPapierSchaar with func GetUserInoerKey & throw exeption by input fault

--- SteenSchaarPapier/Program.cs
+++ SteenSchaarPapier/Program.cs
@@ -4,7 +4,7 @@ namespace SteenSchaarPapier
 {
     internal class Program
     {
-        private const string inputError = "Invoer bevat een fout. Probeer het opnieuw...";
+        private const string messageRepeat = "Probeer het opnieuw";
         private const string inputGebaar = "Bent u een steen(1) of een papier(2) of een schaar(3) ?";
         private const int scoreWinner = 10;
 
@@ -15,24 +15,54 @@ namespace SteenSchaarPapier
         { None, Draw, Win, Loss };
 
         /// <summary>
+        /// Enum with possible users keys from menu Rock Paper Scissors game
+        /// </summary>
+        public enum UserKeys
+        { None = -1, Esc = 27, Steen = 49, Papier = 50, Schaar = 51 };
+
+        /// <summary>
+        /// Enum with random gesture
+        /// </summary>
+        public enum RandomKeys
+        { None = 0, Steen, Papier, Schaar };
+
+        /// <summary>
+        /// Translate inputs user keys to processing keys
+        /// </summary>
+        /// <param name="KeyChar"></param>
+        /// <returns></returns>
+        public static UserKeys GetUserInvoerKey(char KeyChar)
+        {
+            switch (KeyChar)
+            {
+                case ('1'): return UserKeys.Steen;
+                case ('2'): return UserKeys.Papier;
+                case ('3'): return UserKeys.Schaar;
+                case ('\u001b'): return UserKeys.Esc;
+                default: break;
+            }
+            return UserKeys.None;
+        }
+
+        /// <summary>
         /// Get result score from compare two play gestures
         /// </summary>
-        /// <param name="gebaarChar"></param>
+        /// <param name="userInvoer"></param>
         /// <param name="randomInvoer"></param>
         /// <returns></returns>
-        private static SteenSchaarPapier GetResultScore(int gebaarChar, int randomInvoer)
+        private static SteenSchaarPapier GetResultScore(UserKeys userInvoer, int randomInvoer)
         {
-            if ((gebaarChar == 49 && randomInvoer == 1)
-                || (gebaarChar == 50 && randomInvoer == 2)
-                || (gebaarChar == 51 && randomInvoer == 3))
+            if ((userInvoer == UserKeys.Steen && randomInvoer == (int)RandomKeys.Steen)
+                || (userInvoer == UserKeys.Papier && randomInvoer == (int)RandomKeys.Papier)
+                || (userInvoer == UserKeys.Schaar && randomInvoer == (int)RandomKeys.Schaar))
             { return SteenSchaarPapier.Draw; }
-            if ((gebaarChar == 49 && randomInvoer == 2)
-                || (gebaarChar == 50 && randomInvoer == 3)
-                || (gebaarChar == 51 && randomInvoer == 1))
+            if ((userInvoer == UserKeys.Steen && randomInvoer == (int)RandomKeys.Papier)
+                || (userInvoer == UserKeys.Papier && randomInvoer == (int)RandomKeys.Schaar)
+                || (userInvoer == UserKeys.Schaar && randomInvoer == (int)RandomKeys.Steen))
             { return SteenSchaarPapier.Loss; }
-            if ((gebaarChar == 49 && randomInvoer == 3)
-                || (gebaarChar == 50 && randomInvoer == 1)
-                || (gebaarChar == 51 && randomInvoer == 2))
+            if ((userInvoer == UserKeys.Steen && randomInvoer == (int)RandomKeys.Schaar)
+                || (userInvoer == UserKeys.Papier && randomInvoer == (int)RandomKeys.Steen)
+                || (userInvoer == UserKeys.Schaar && randomInvoer == (int)RandomKeys.Papier))
             { return SteenSchaarPapier.Win; }
             return SteenSchaarPapier.None;
         }
@@ -40,18 +70,26 @@ namespace SteenSchaarPapier
         private static void Main(string[] args)
         {
             ConsoleKeyInfo cki_Key;
-            bool isInputCorrect = true;
+            bool isProcessing = true;
             Random random = new Random();
             int scoreUser = 0;
             int scoreRandom = 0;
-            while (scoreUser != scoreWinner && scoreRandom != scoreWinner && isInputCorrect)
+            Console.WriteLine("Klik op ESC om op elk moment te kunnen stoppen");
+            while (scoreUser != scoreWinner && scoreRandom != scoreWinner && isProcessing)
                 try
                 {
                     Console.Write(inputGebaar);
                     cki_Key = Console.ReadKey();
+                    UserKeys invoer = GetUserInvoerKey(cki_Key.KeyChar);
+                    if (invoer == UserKeys.Esc)
+                    {
+                        isProcessing = false;
+                        Console.WriteLine();
+                        throw new Exception("Processing is gestopt. " + messageRepeat + " later");
+                    }
                     int randomgetaal = random.Next(1, 4);
 
-                    SteenSchaarPapier result = GetResultScore((int)cki_Key.KeyChar, randomgetaal);
+                    SteenSchaarPapier result = GetResultScore(invoer, randomgetaal);
                     switch (result)
                     {
                         case (SteenSchaarPapier.Win):
@@ -73,20 +111,17 @@ namespace SteenSchaarPapier
                             }
                         case (SteenSchaarPapier.None):
                             {
-                                Console.WriteLine(inputError);
-                                isInputCorrect = false;
-                                break;
+                                throw new Exception("Een verkeerde toets was gedrukt. " + messageRepeat);
                             }
                         default:
                             {
-                                break;
+                                throw new Exception("Een verkeerde toets was gedrukt. " + messageRepeat);
                             }
                     }
                 }
                 catch (Exception e)
                 {
-                    isInputCorrect = false;
-                    Console.WriteLine(inputError);
+                    Console.WriteLine(e.Message);
                 }
             Console.WriteLine($"Game over. Score User {scoreUser} : {scoreRandom} Computer");
         }
